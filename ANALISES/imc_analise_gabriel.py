cb311530328c21d9e30035584e5b840d3d09c5eb
GabrielAbad <125308525+GabrielAbad@users.noreply.github.com>
2023-10-11 19:21:05 -0300

    Add files via upload

---
+++ ANALISES/imc_analise_gabriel.py
@@ -0,0 +1,8 @@
+import sys
+
+# Trocar o que esta entre aspas para o caminho em que o repositorio do projeto esta salvo no seu pc.
+sys.path.append('C:/Users/Home/GovDataProj')
+
+import utilsgabriel as ug
+
+ug.bar_plot_imc()

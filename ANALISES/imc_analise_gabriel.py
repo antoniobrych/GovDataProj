b7422877806bc9a8a7c512335160b134b0570e1c
GabrielAbad <125308525+GabrielAbad@users.noreply.github.com>
2023-10-12 17:05:53 -0300

    Delete ANALISES directory

--- ANALISES/imc_analise_gabriel.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-# Trocar o que esta entre aspas para o caminho em que o repositorio do projeto esta salvo no seu pc.
-sys.path.append('C:/Users/Home/GovDataProj')
-
-import utilsgabriel as ug
-
-ug.bar_plot_imc()

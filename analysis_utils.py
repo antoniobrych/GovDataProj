4f0dbbb4a4989e1a76c31a1f9b3103a560b56340
Antonio Brych <antoniosouzabrych@gmail.com>
2023-10-12 19:45:33 -0300

    utils developed

--- analysis_utils.py
+++ analysis_utils.py
@@ -9,3 +9,25 @@ Então, esse módulo contém funções específicas para uma
 
 abordagem de séries temporais nessa base.
 """
+import pandas as pd
+
+def yearly_mean(df:pd.DataFrame)->pd.DataFrame:
+    """
+    Função que cálcula a média anual de variáveis numéricas
+
+    no dataset SERMIL (Concatenado ou não.)   
+    Returns:
+    -------
+    pandas.DataFrame:
+        Da
+    """
+    
+    selected_df = df[['VINCULACAO_ANO', 'CINTURA', 'PESO', 'ALTURA']]
+    
+    selected_df = selected_df.set_index('VINCULACAO_ANO')
+    
+    # Grouping the data by year, and calculating the mean afterwwards.
+    selected_df = selected_df.groupby(level='VINCULACAO_ANO').mean()
+
+    # Return the resulting DataFrame, which contains the yearly mean values.
+    return selected_df

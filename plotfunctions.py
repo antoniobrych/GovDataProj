8727d73c08addc53965e2c15a3293e6334f75b82
Henrique Borges Carvalho <126023248+HenriqueBorges6@users.noreply.github.com>
2023-10-12 11:10:00 -0300

    função de gerar barplot: organizei, tornei mais geral pro uso

--- plotfunctions.py
+++ plotfunctions.py
@@ -1,18 +1,43 @@
 # Funções que geram os gráficos
 
-def gerar_grafico_barras_agrupadas(df):
-    import matplotlib as plt
-    import pandas as pd
+def bar_cluster(df, column1, column2, name, xname, yname):
+    """
+    Gera um gráfico de barras agrupadas.
 
+    Parâmetros
+    ----------
+    df : pandas.core.frame.DataFrame
+        O DataFrame que contém os dados.
+    column1 : str
+        O nome da primeira coluna para agrupamento.
+    column2 : str
+        O nome da segunda coluna para agrupamento.
+    name : str
+        O título do gráfico.
+    xname : str
+        O rótulo do eixo x.
+    yname : str
+        O rótulo do eixo y.
+
+    Retorna
+    -------
+    None
+        O gráfico é exibido, não há valor de retorno.
+    """
+    import pandas as pd
+    import matplotlib as plt
+    
     plt.figure(figsize=(10, 6))
     
-    # crosstab para contar as ocorrências de escolaridade e dispensa
-    tabela_contagem = pd.crosstab(df['ESCOLARIDADE'], df['Dispensa'])
+    # crosstab para contar as ocorrências das colunas desejadas
+    tabela_contagem = pd.crosstab(df[column1], df[column2])
     
+    #criar plot com essas colunas
     tabela_contagem.plot(kind='bar')
     
-    plt.title('Escolaridade dos Alistados com e sem dispensa')
-    plt.xlabel('Escolaridade')
-    plt.ylabel('Número de Pessoas')
+    plt.title(name)
+    plt.xlabel(xname)
+    plt.ylabel(yname)
     
     plt.show()
+    

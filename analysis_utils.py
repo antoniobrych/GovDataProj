5c431714d2a192e1f4a7f05bafbfe5968eab9ef0
Antonio Brych <antoniosouzabrych@gmail.com>
2023-10-12 19:36:40 -0300

    utils defined new utils

---
+++ analysis_utils.py
@@ -0,0 +1,11 @@
+"""
+Principal objetivo da minha análise
+
+é estudar a mudança ao longo dos anos nessa base de dados.
+
+Principalmente Peso, Altura, Tam. Cabeça,Cintura etc.
+
+Então, esse módulo contém funções específicas para uma
+
+abordagem de séries temporais nessa base.
+"""
--- viz.py
+++ viz.py
@@ -1,13 +1,15 @@
 """
 
 Data Viz
+
+
 """
 
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-requested_columns = ['PESO','ALTURA','CALCADO','CABECA','CINTURA']    
+requested_columns = ['PESO','ALTURA','CALCADO','CABECA','CINTURA']
 
 df = pd.read_csv("data_concat//SERMIL_5_ANOS.csv",usecols=requested_columns)
 

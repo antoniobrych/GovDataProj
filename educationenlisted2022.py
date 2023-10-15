254fdaab0e25e8f67168a8ebe21d0d9109458814
GabrielAbad <125308525+GabrielAbad@users.noreply.github.com>
2023-10-14 22:40:56 -0300

    Add files via upload

---
+++ educationenlisted2022.py
@@ -0,0 +1,25 @@
+from utils_henrique import take_data, transform_column
+from plotfunctions import bar_cluster
+
+#Pegando as colunas desejadas para a análise
+data_frame = take_data("sermil2022.csv",["ESCOLARIDADE","DISPENSA"])
+
+#criando dicionário para usar como arg em transform_columns
+transform_dict = {
+    'Ensino Superior': 'Superior',
+    'Ensino Médio': 'Médio',
+    'Ensino Fundamental': 'Fundamental',
+    "Pós-":"Superior",
+    "Mestrado":"Superior",
+    "Doutorado":"Superior"
+}
+
+# transformando os dados da coluna "ESCOLARIDADE"
+data_transf = transform_column(data_frame,"ESCOLARIDADE", transform_dict)
+# agora teremos apenas os valores "Analfabeto","Alfabetizado","Fundamental","Médio","Superior"
+# Fundamental --> Todos que completaram ou que ainda estão no Ensino Fundamental
+# Médio --> Todos que completaram ou que ainda estão no Ensino Médio
+# Superio --> Todos que completaram ou estão em algum curso superior, pós-graduação, mestrado, doutarado, pós-doutorado ...
+
+# Agora plotamos
+bar_cluster(data_transf,"ESCOLARIDADE","DISPENSA","Escolaridade dos alistados","nível de escolaridade","qtd alistados")

57625412acfc1f258805420f83501b45b8747a89
GabrielAbad <125308525+GabrielAbad@users.noreply.github.com>
2023-10-14 22:42:55 -0300

    Update ageenlisted2022.py

--- ageenlisted2022.py
+++ ageenlisted2022.py
@@ -1,7 +1,7 @@
-from utils_henrique import take_data, calculate_age
-from plotfunctions import top_ages_histogram
+from utils import utils_henrique as uh
+from utils import plotfunctions as pf
 
-df = take_data("sermil2022.csv",["ANO_NASCIMENTO"])
-data = calculate_age(df,"ANO_NASCIMENTO")
+df = uh.take_data("sermil2022.csv",["ANO_NASCIMENTO"])
+data = uh.calculate_age(df,"ANO_NASCIMENTO")
 
-top_ages_histogram(data,"Idade",4,colors= ['#123456', '#6d745f', "black","black","black"])
+pf.top_ages_histogram(data,"Idade",4,colors= ['#123456', '#6d745f', "black","black","black"])
